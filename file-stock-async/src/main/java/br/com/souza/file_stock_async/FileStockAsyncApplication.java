d282da36c684c8b695dfe03078f5b17e70efe810
Gilson Souza <br.gilson.sp@gmail.com>
2024-10-11 22:44:00 -0300

    start java project arch ports and adapters

---
+++ file-stock-async/src/main/java/br/com/souza/file_stock_async/FileStockAsyncApplication.java
@@ -0,0 +1,13 @@
+package br.com.souza.file_stock_async;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileStockAsyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileStockAsyncApplication.class, args);
+	}
+
+}
---
+++ file-stock-async/src/test/java/br/com/souza/file_stock_async/FileStockAsyncApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.souza.file_stock_async;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileStockAsyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
